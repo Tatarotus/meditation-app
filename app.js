76ab6a65bbf1b65f1ea48a7f9de8eb57eb5a15b2
Sam Dantas <contato.ashanti@gmail.com>
2019-04-03 20:15:03 -0300

    Added play/pause icon toggle

--- app.js
+++ app.js
@@ -1,9 +1,22 @@
 window.addEventListener('DOMContentLoaded', function(event) {
   console.log('DOM fully loaded and parsed');
-  console.log(event);
-  const soundPicker = document.querySelector('.sound-picker');
 
-  soundPicker.addEventListener('click', () => {
-    alert('I was clicked');
-  })
+  //DOM variables
+  const playSvg = document.querySelector('.play');
+  const pauseSvg = document.querySelector('.pause');
+  const playPauseBtn = document.querySelector('.play-pause-btn');
+
+  let play = false;
+
+  playPauseBtn.addEventListener('click', function() {
+    if (!play) {
+      play = true;
+      playSvg.style.display = 'none';
+      pauseSvg.style.display = 'flex';
+    } else {
+      play = false;
+      playSvg.style.display = 'flex';
+      pauseSvg.style.display = 'none';
+    }
+  });
 });
