73f95bc19c2b189584eed29c6f1de5edbef875e3
Sam Dantas <contato.ashanti@gmail.com>
2019-04-05 16:14:52 -0300

    Change time with DOM buttons, display remaining time and stops when it gets to zero

--- app.js
+++ app.js
@@ -8,8 +8,10 @@ window.addEventListener('DOMContentLoaded', function(event) {
   const videoBg = document.querySelector('.default-bg');
   const song = document.querySelector('.song');
   const changeBgBtns = document.querySelectorAll('.sound-picker button');
+  const time = document.querySelector('.time');
+  const durationBtns = document.querySelectorAll('.time-select button');
 
-
+  let duration = 120;
   let play = false;
   let videoSrc; //Later change this variable thought button press
   let audioSrc; //Later change this variable thought button press
@@ -21,10 +23,10 @@ window.addEventListener('DOMContentLoaded', function(event) {
       play = true;
       playSvg.style.display = 'none';
       pauseSvg.style.display = 'flex';
-      
+
       song.play();
       videoBg.play();
-    //Stop video from playing
+      //Stop video from playing
     } else {
       //Change logic and change the pause icon to play
       play = false;
@@ -42,13 +44,13 @@ window.addEventListener('DOMContentLoaded', function(event) {
 
   //Change Bg and Audio Event
   changeBgBtns.forEach(bgOption => {
-    bgOption.onclick = function () {
+    bgOption.onclick = function() {
       //Get the video URL
       videoSrc = bgOption.getAttribute('data-video');
       //Set video URL
       videoBg.firstElementChild.setAttribute('src', videoSrc);
       //Get audio URL
-      audioSrc = (this.getAttribute('data-sound'));
+      audioSrc = this.getAttribute('data-sound');
       //Set audio URL
       song.firstElementChild.setAttribute('src', audioSrc);
       //Load audio and video
@@ -58,7 +60,47 @@ window.addEventListener('DOMContentLoaded', function(event) {
       if (play === true) {
         playerHandler();
       }
-    }
-  })
+    };
+  });
 
+  function updateTimer() {
+    let remainingTime = duration - song.currentTime;
+    let min = ~~((remainingTime % 3600) / 60);
+    let secs = ~~(remainingTime % 60);
+    console.log(min, secs);
+    if (remainingTime > 0) {
+      time.innerHTML = `${min}:${secs}`;
+      //#1 checks if it is more than 1 min and adds a leading 0 if seconds is less than 10
+      //#2 checks if it is less than 1 (there is no minute = 0) and only displays seconds
+      //#3 if it's more than one minute and more than 10 seconds display time normaly
+      time.innerHTML = min >=1 && secs < 10 ? min + ':0' + secs : min === 0 ? secs : `${min}:${secs}`;
+    } else {
+      videoBg.load();
+      song.load();
+      playerHandler();
+      stop();
+    }
+  }
+  //Get the time left
+  song.addEventListener('timeupdate', () => {
+    updateTimer();
+  });
+
+  durationBtns.forEach(btn => {
+    btn.addEventListener('click', () => {
+      let newDuration = parseInt(btn.getAttribute('data-time'));
+
+      switch (newDuration) {
+        case 120:
+          duration = newDuration;
+          break;
+        case 300:
+          duration = newDuration;
+          break;
+        case 600:
+          duration = newDuration;
+          break;
+      }
+    });
+  });
 });
